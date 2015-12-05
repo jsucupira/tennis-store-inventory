7cb2ca8b4dc8ed21a8a07a792e8836ef85cf2d95
Jonathas Sucupira <jonathas@jsucupira.com>
2015-12-04 22:32:27 -0500

    Adding more mock context

---
+++ TestHelpers/Mocks/InMemoryStoreContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Domain.MasterData.ProductAggregate;
+using Domain.MasterData.StoreAggregate;
+
+namespace TestHelpers.Mocks
+{
+    [Export(typeof (IStoreContext))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class InMemoryStoreContext : IStoreContext
+    {
+        private readonly InMemoryStorage<Store, Guid> _inMemoryStorage = new InMemoryStorage<Store, Guid>();
+
+        public Store Get(Guid id)
+        {
+            return _inMemoryStorage.Get(id);
+        }
+
+        public List<Store> FindAll()
+        {
+            return _inMemoryStorage.FindAll();
+        }
+
+        public Store Create(Store entity)
+        {
+            return _inMemoryStorage.Create(entity);
+        }
+
+        public void Update(Store entity)
+        {
+            _inMemoryStorage.Update(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            _inMemoryStorage.Delete(id);
+        }
+    }
+}
---
+++ TestHelpers/Mocks/InMemoryVendorContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Domain.MasterData.ProductAggregate;
+using Domain.MasterData.VendorAggregate;
+
+namespace TestHelpers.Mocks
+{
+    [Export(typeof (IVendorContext))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class InMemoryVendorContext : IVendorContext
+    {
+        private readonly InMemoryStorage<Vendor, string> _inMemoryStorage = new InMemoryStorage<Vendor, string>();
+
+        public Vendor Get(string id)
+        {
+            return _inMemoryStorage.Get(id);
+        }
+
+        public List<Vendor> FindAll()
+        {
+            return _inMemoryStorage.FindAll();
+        }
+
+        public Vendor Create(Vendor entity)
+        {
+            return _inMemoryStorage.Create(entity);
+        }
+
+        public void Update(Vendor entity)
+        {
+            _inMemoryStorage.Update(entity);
+        }
+
+        public void Delete(string id)
+        {
+            _inMemoryStorage.Delete(id);
+        }
+    }
+}
